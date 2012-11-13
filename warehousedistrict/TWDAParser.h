5c6b9c37adfc44b386564a811ea3b258a53dd3e4
Don Miller <donmiller@Don-Miller-MBP-Retina.local>
2012-11-12 20:31:21 -0500

    Added parsing class

---
+++ warehousedistrict/TWDAParser.h
@@ -0,0 +1,13 @@
+//
+//  TWDAParser.h
+//  warehousedistrict
+//
+//  Created by Don Miller on 11/12/12.
+//  Copyright (c) 2012 GroundSpeed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TWDAParser : NSObject
+
+@end
---
+++ warehousedistrict/TWDAParser.m
@@ -0,0 +1,13 @@
+//
+//  TWDAParser.m
+//  warehousedistrict
+//
+//  Created by Don Miller on 11/12/12.
+//  Copyright (c) 2012 GroundSpeed. All rights reserved.
+//
+
+#import "TWDAParser.h"
+
+@implementation TWDAParser
+
+@end
